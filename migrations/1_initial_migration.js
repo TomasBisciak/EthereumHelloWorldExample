461e783f895e882efa98bdde76781069d97afd30
Tomas Bisciak <tomasbisciak@gmail.com>
2017-12-31 21:45:56 +0100

    Removed deploy of helloWorld contract from initial_migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,5 @@
 var Migrations = artifacts.require("./Migrations.sol");
-var HelloWorld = artifacts.require("./HelloWorld.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
-  deployer.deploy(HelloWorld);
 };
